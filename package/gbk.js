bdb676f37ffdd9b81fdc9a7f6ad5e00ec29183fb
hdlr <95166106+hhd2009@users.noreply.github.com>
2022-11-21 16:14:30 +0800

    Update gbk.js

--- package/gbk.js
+++ package/gbk.js
@@ -1,17 +1,26 @@
-function urldecode(str, charset, callback) {
-
-window._urlDecodeFn_ = callback;
-
-var script = document.createElement('script');
-
-script.id = '_urlDecodeFn_';
-
-var src = 'data:text/javascript;charset=' + charset + ',_urlDecodeFn_("' + str + '");'
-
-src += 'document.getElementById("_urlDecodeFn_").parentNode.removeChild(document.getElementById("_urlDecodeFn_"));';
-
-script.src = src;
-
-document.body.appendChild(script);
-
+function gbkEncode(str) {
+  return str.replace(/./g, function (a) {
+    var code = a.charCodeAt(0);
+    if (isAscii(code)) {
+      return encodeURIComponent(a);
+    } else {
+      var key = code.toString(16);
+      if (key.length != 4)key = ('000' + key).match(/....$/)[0];
+      return U2Ghash[key] || a;
+    }
+  });
+}
+ 
+function gbkDecode(str, callback) {
+  return str.replace(/%[0-9A-F]{2}%[0-9A-F]{2}/g, function (a) {
+    if (a in G2Uhash) {
+      return String.fromCharCode('0x' + G2Uhash[a]);
+    } else {
+      return a;
+    }
+  }).replace(/%[\w]{2}/g, function (a) {
+ 
+    return decodeURIComponent(a);
+ 
+  });
 }
