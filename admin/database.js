8f400786f96b77e4805a94a15f5d03908929bc32
hdlr <95166106+hhd2009@users.noreply.github.com>
2022-11-25 15:22:01 +0800

    Update database.js

--- admin/database.js
+++ admin/database.js
@@ -1,6 +1,6 @@
 var database='{\
-  "data":["4d7d46f341ef6e354d07fa81a7384706","6000af5e9d3b5b909d1f21637e3f9274","3a68d6c9abb1ab8a2ffe536736026ee1","52b93601607bf5cf3618d4d10a88f160","244e379f6672d578a9a3fd569471aa5a","91200e1700603b5f76b460dfc79959af"],\
-  "text":["寒冬利刃",                                                           "海拥",                                                               "侯添羿"]\
+  "data":["4d7d46f341ef6e354d07fa81a7384706","6000af5e9d3b5b909d1f21637e3f9274","3a68d6c9abb1ab8a2ffe536736026ee1","52b93601607bf5cf3618d4d10a88f160","244e379f6672d578a9a3fd569471aa5a","91200e1700603b5f76b460dfc79959af","7e5c715121efb3e30106cc80e8800884","00b1dc34dc0236bc4e3d44ca602acc18"],\
+  "text":["寒冬利刃",                                                           "海拥",                                                               "侯添羿",                                                              "胡诗钰"]\
 }'
 
 function read_db(){
