5610b25dba7bbd45fb43dc354978f6f752300530
hdlr <95166106+hhd2009@users.noreply.github.com>
2022-11-25 08:50:55 +0800

    Create login.js

---
+++ admin/login.js
@@ -0,0 +1,38 @@
+function set_cookie(name, value, daysToLive) {
+    // 对 cookie 值进行编码以转义其中的分号、逗号和空格
+    var cookie = name + "=" + encodeURIComponent(value);
+    cookie += "; path=/"
+    if(typeof daysToLive === "number") {
+        /* 设置 max-age 属性 */
+        cookie += "; max-age=" + (daysToLive*24*60*60);
+    }
+    document.cookie = cookie;
+}
+
+function get_cookie(name) {
+    // 拆分 cookie 字符串
+    var cookieArr = document.cookie.split(";");
+   
+    // 循环遍历数组元素
+    for(var i = 0; i < cookieArr.length; i++) {
+        var cookiePair = cookieArr[i].split("=");
+       
+        /* 删除 cookie 名称开头的空白并将其与给定字符串进行比较 */
+        if(name == cookiePair[0].trim()) {
+            // 解码cookie值并返回
+            return decodeURIComponent(cookiePair[1]);
+        }
+    }
+    // 如果未找到，则返回null
+    return null;
+}
+
+function del_cookie(name) {
+    set_cookie(name,null,0)
+}
+
+var user=document.getElementById("user_").value
+var password=document.getElementById("password_").value
+set_cookie('user',user,5)
+set_cookie('password',password,5)
+			
