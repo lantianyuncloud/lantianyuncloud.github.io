c0fb78afe4877478423cea4f9d8d8b7b112dbdf8
hdlr <95166106+hhd2009@users.noreply.github.com>
2022-11-25 13:56:21 +0800

    Update database.js

--- admin/database.js
+++ admin/database.js
@@ -1,9 +1,7 @@
-var database='[\
-  {\
-    "data":["4d7d46f341ef6e354d07fa81a7384706","6000af5e9d3b5b909d1f21637e3f9274"],\
-    "text":["寒冬利刃"]\
-  }\
-]'
+var database='{\
+  "data":["4d7d46f341ef6e354d07fa81a7384706","6000af5e9d3b5b909d1f21637e3f9274"],\
+  "text":["寒冬利刃"]\
+}'
 
 function read_db(){
   return database
