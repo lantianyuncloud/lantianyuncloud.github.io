f20b79c139ebb1b4531b85b08b50b2cccad0cecd
hdlr <95166106+hhd2009@users.noreply.github.com>
2022-11-25 08:18:37 +0800

    Update checkuser.js

--- admin/checkuser.js
+++ admin/checkuser.js
@@ -37,6 +37,6 @@ function checkuser(){
   var user=get_cookie('user')
   var password=get_cookie('password')
   if(user==null||password==null) {
-    window.location.href="\./admin/login"
+    window.location.href="\\./admin/login"
   }
 }
